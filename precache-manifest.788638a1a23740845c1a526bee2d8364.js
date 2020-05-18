d615b0037cd1a045ca91e1f0f818cebc068467ff
jossAcosta <josselineacostamtz@gmail.com>
2020-05-18 09:44:44 -0500

    Updates

---
+++ precache-manifest.788638a1a23740845c1a526bee2d8364.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fc557251ee06682ffb4d2a3cfd93318",
+    "url": "/blog/index.html"
+  },
+  {
+    "revision": "3c7de45d195e066b06da",
+    "url": "/blog/static/css/main.c711415c.chunk.css"
+  },
+  {
+    "revision": "68fdf8de607cb9cbca58",
+    "url": "/blog/static/js/2.15fe24e2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/blog/static/js/2.15fe24e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c7de45d195e066b06da",
+    "url": "/blog/static/js/main.35f65b06.chunk.js"
+  },
+  {
+    "revision": "429b4471f1d66eff61db",
+    "url": "/blog/static/js/runtime-main.f97e9f90.js"
+  }
+]);
